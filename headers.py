6a52e105dcde11a59e36bdbf49d89cd4401a60c1
Youssef Ait Oughlad <youssefaitoughlad374@gmail.com>
2026-04-09 12:09:51 +0100

    change_header.py

--- headers.py
+++ headers.py
@@ -1,58 +1,39 @@
 import time
+from pathlib import Path
 from os import system
 
 
-def type_writer(text, delay=0.002):
+def type_writer(text: str, delay: float = 0.002) -> None:
     for char in text:
         print(char, end='', flush=True)
         time.sleep(delay)
     print()
 
 
-amazing_banner = r"""
-                         █████╗ ███╗   ███╗ █████╗ ███████╗██╗███╗   ██╗ ██████╗ 
-                        ██╔══██╗████╗ ████║██╔══██╗╚══███╔╝██║████╗  ██║██╔════╝ 
-                        ███████║██╔████╔██║███████║  ███╔╝ ██║██╔██╗ ██║██║  ███╗
-                        ██╔══██║██║╚██╔╝██║██╔══██║ ███╔╝  ██║██║╚██╗██║██║   ██║
-                        ██║  ██║██║ ╚═╝ ██║██║  ██║███████╗██║██║ ╚████║╚██████╔╝
-                        ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝               
+amazing_banner: str = Path("amazing_banner.txt").read_text()
 
-
-                                    CREATED BY YAIT-OUG & ILHAKAM
-
-"""
-
-intro_lines = [
+intro_lines: list[str] = [
     "Welcome to the AMAZING PROJECT!",
     "Prepare to witness something extraordinary...",
     "Ideas come alive here, and innovation has no limits.",
-    "Let’s embark on this incredible journey together!\n",
+    "Let's embark on this incredible journey together!\n",
     "Loading features...",
-    "READY 🚀"
+    "READY 🚀",
 ]
 
-goodby_banner = r"""
-                     ██████╗ ██╗  ██╗ █████╗ ██╗   ██╗ █████╗ ██████╗ ███████╗██╗  ██╗ █████╗ 
-                    ██╔════╝ ██║  ██║██╔══██╗╚██╗ ██╔╝██╔══██╗██╔══██╗██╔════╝██║  ██║██╔══██╗
-                    ██║  ███╗███████║███████║ ╚████╔╝ ███████║██████╔╝█████╗  ███████║███████║
-                    ██║   ██║██╔══██║██╔══██║  ╚██╔╝  ██╔══██║██╔══██╗██╔══╝  ██╔══██║██╔══██║
-                    ╚██████╔╝██║  ██║██║  ██║   ██║   ██║  ██║██║  ██║███████╗██║  ██║██║  ██║
-                     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝
- """
+goodby_banner: str = Path("goodby_banner.txt").read_text()
+
 
 def show_amazing_banner() -> None:
-    system("clear")    
+    system("clear")
     for line in amazing_banner.split("\n"):
         type_writer(line)
     for line in intro_lines:
         type_writer(line, 0.03)
-    
+
 
 def show_goodby_banner() -> None:
     system('clear')
     for line in goodby_banner.split("\n"):
         type_writer(line)
 
-
-show_amazing_banner()
-show_goodby_banner()
